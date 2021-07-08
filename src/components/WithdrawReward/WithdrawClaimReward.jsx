ca68e7ff44ec6d95dd1ecf450ea9008807378e00
Lucas Matheus <lucasmatheus2021@gmail.com>
2021-07-08 16:19:57 -0300

    feat: paypal and pix button styles
    Signed-off-by: Lucas Matheus <lucasmatheus2021@gmail.com>

--- src/components/WithdrawReward/WithdrawClaimReward.jsx
+++ src/components/WithdrawReward/WithdrawClaimReward.jsx
@@ -1,6 +1,9 @@
 import {
 	Div,
-	PixIcon
+	PixIcon,
+	TitleArea,
+	FormOfPayment,
+	Button
 } from '../../styles/components/WithdrawClaimReward';
 import { Tooltip } from '@material-ui/core';
 import {
@@ -18,16 +21,29 @@ const WithdrawClaimReward = ({
 }) => {
 	return (
 		<Div className="container">
-			<div>
-				<div>
-					<RiPaypalFill />
+			<FormOfPayment>
+				<TitleArea>
+					<h3>Formas de retirada</h3>
+				</TitleArea>
+				<Button
+					hoverColor={'rgba(0, 150, 218, .212)'}
+					hoverBorderColor={'rgba(0, 150, 218, .312)'}
+					disabled
+					comingSoon
+				>
+					<RiPaypalFill size={18} />
 					<span>Paypal</span>
-				</div>
-				<div>
+				</Button>
+				<Button
+					hoverColor={'rgba(48, 182, 168, .212)'}
+					hoverBorderColor={'rgba(48, 182, 168, .312)'}
+					disabled
+					comingSoon
+				>
 					<PixIcon src={'/assets/icons/pix_icon.png'} alt='pix icon' />
 					<span>Pix</span>
-				</div>
-			</div>
+				</Button>
+			</FormOfPayment>
 		</Div>
 	);
 }
--- src/styles/components/WithdrawClaimReward.jsx
+++ src/styles/components/WithdrawClaimReward.jsx
@@ -7,6 +7,64 @@ export const Div = styled.div`
 `;
 
 export const PixIcon = styled.img`
-	width: 16px;
-	height: 16px;
+	width: 17px;
+	height: 17px;
+	pointer-events: none;
+`;
+
+export const TitleArea = styled.div`
+	h3 {
+		font-size: 16px;
+		color: #333;
+	}
+`;
+
+export const FormOfPayment = styled.div`
+	width: 444px;
+	margin-top: 50px;
+	@media screen and (max-width: 620px) {
+		width: 256px;
+	}
+`;
+
+export const Button = styled.div`
+	display: flex;
+	align-items: center;
+	color:#4c4c4c;
+	background: #f9f9f9;
+	margin: 10px auto;
+	width: 100%;
+	padding: 6px 20px;
+	cursor: pointer;
+	user-select: none;
+	border-radius: 4px;
+	border: 1px solid transparent;
+	position: relative;
+	
+	${props => props.disabled && `
+		background: #e0e0e0;
+		cursor: default;
+		&:hover {
+			background: #e0e0e0 !important;
+			border: 1px solid transparent !important;
+		}
+	`}
+	${props => props.comingSoon && `
+		&::after {
+			content: '(em breve)';
+			position: absolute;
+			right: 15px;
+		}
+	`}
+
+	&:hover {
+		background: ${props => props.hoverColor};
+		border: 1px solid ${props => props.hoverBorderColor};
+	}
+	span {
+		font-size: 16px;
+		font-weight: 400;
+		font-style: italic;
+		margin: auto 10px;
+	}
 `;
